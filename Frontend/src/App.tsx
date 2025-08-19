b2c81ac148430a78543429739e610e40bd74c226
Abhigyan Raj <abhigyanraj@ABHIGYAN.local>
2025-08-19 21:20:55 +0530

    Initial commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div className='h-screen w-screen bg-black flex justify-center items-center'>
+        <h1 className='text-white text-base text-black'>Welcome to Seedora</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
